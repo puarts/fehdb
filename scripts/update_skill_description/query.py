75170931f7e5c2e0bde93549b22da44691ce3dbc
yakitori776 <yakitori776@gmail.com>
2024-10-14 14:48:46 +0900

    スクリプトに--dry-runオプションを追加

--- scripts/update_skill_description/query.py
+++ scripts/update_skill_description/query.py
@@ -1,3 +1,4 @@
+import argparse
 import sqlite3
 from typing import List, Tuple
 from parse_file import parse_file
@@ -47,16 +48,48 @@ def parse_field(parsed: List[Tuple[str, str]]) -> List[Tuple[str, str, str, str,
 
 
 def main():
+    # 引数パーサを作成
+    parser = argparse.ArgumentParser(description="Dry run flag example")
+
+    # `--dry-run` 引数をオプションとして追加（指定しないと `False` になる）
+    parser.add_argument('--dry-run', action='store_true', help="Perform a dry run without making changes")
+
+    # 引数を解析
+    args = parser.parse_args()
+
+    # dry-run フラグが設定されていれば True、されていなければ False
+    dry_run = args.dry_run
+
     # データベースに接続する（存在しない場合は作成される）
     conn = sqlite3.connect('./../../feh-skills.sqlite3')
 
-    data_to_insert = parse_file('./../../sources/skill-desc/8-10-4-2.txt')
+    data_to_insert = parse_file('./../../sources/skill-desc/8-10-15.txt')
     # data_to_insert = parse_file('./../../sources/skill-desc/refine-2024-10.txt')
     # データを挿入する
-    insert_data(conn, data_to_insert)
+    if not dry_run:
+        insert_data(conn, data_to_insert)
+    else:
+        index_list = list(map(lambda x: int(x[0].split('-')[0]), data_to_insert))
+        if not is_incrementing_by_one(index_list):
+            print(f"[WARN] インデックスが順番になっていません: {index_list}")
+        print('[変換結果]')
+        print('\n'.join(map(str, data_to_insert)))
 
     # データベース接続を閉じる
     conn.close()
+
+
+def is_incrementing_by_one(lst):
+    # リストが空または1つの要素だけの場合は+1ずつ増えているとみなす
+    if len(lst) < 2:
+        return True
+
+    # 隣接する要素の差が1かどうかをチェック
+    for i in range(1, len(lst)):
+        if lst[i] - lst[i - 1] != 1:
+            return False
+
+    return True
 
 
 if __name__ == '__main__':
