ea0a29da10b7c51436ab0bbc73acf79a89dc6dbf
puarts <71858166+puarts@users.noreply.github.com>
2025-02-04 19:34:34 +0900

    DB更新

--- update-cyl-vote.bat
+++ update-cyl-vote.bat
@@ -1,4 +1,6 @@
 set vote_root=%~dp0sources\cyl-vote
 set target_cyl=cyl9
-sqlite3 %~dp0feh-cyl.sqlite3 < %vote_root%\%target_cyl%.query
+pushd %vote_root%
+%~dp0externals\sqlite3\sqlite3.exe %~dp0feh-cyl.sqlite3 < %vote_root%\%target_cyl%.query
+popd
 pause
