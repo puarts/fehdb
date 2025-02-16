c61f11935819879bec77e450f9935b2f26283314
yakitori776 <yakitori776@gmail.com>
2025-02-16 20:00:55 +0900

    自動登録を追加

--- scripts/update_skill_description/parse_file.py
+++ scripts/update_skill_description/parse_file.py
@@ -64,6 +64,8 @@ def get_field_dict_from_description(descriptions: List[str]) -> dict:
             result['counteratk_count'] = 2
         elif description == '飛行特効':
             result['effective'] = '|飛行|'
+        elif description == '騎馬特効':
+            result['effective'] = '|騎馬|'
         elif description == '魔法特効':
             result['effective'] = '|魔法|'
         elif description == '飛行特効無効':
